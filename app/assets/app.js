012fd70afdb794d6ad6538c2de4572ea497ba1db
Ruben Escudero <rubenescudero00@gmail.com>
2022-07-10 11:08:00 +0200

    feat: create first react controller

--- app/assets/app.js
+++ app/assets/app.js
@@ -10,3 +10,48 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Items from './components/items';
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            entries: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts/')
+            .then(response => response.json())
+            .then(entries => {
+                this.setState({
+                    entries
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {this.state.entries.map(
+                    ({ id, title, body }) => (
+                        <Items
+                            key={id}
+                            title={title}
+                            body={body}
+                        >
+                        </Items>
+                    )
+                )}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
